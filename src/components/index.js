aade39fd2aeaf0a6fc3a0dad252466db3fc6d145
Aniket Dey <meaniket1025@gmail.com>
2025-07-10 12:45:55 +0530

    feat: add Login and PostCard components, and implement Select component with options

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
-import Header from "./header/Header";
+import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from './Input'
 
 export {
     Container,
@@ -10,4 +11,5 @@ export {
     Footer,
     Logo,
     LogoutBtn,
+    Input
 }
